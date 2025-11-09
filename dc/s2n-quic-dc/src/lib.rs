cffbdf875291cfb3c776040f46fe7cd2b637a5b4
Cameron Bytheway <bythewc@amazon.com>
2025-11-09 11:31:39 -0700

    add plan

--- dc/s2n-quic-dc/src/lib.rs
+++ dc/s2n-quic-dc/src/lib.rs
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+pub mod allocator;
 pub mod clock;
 pub mod congestion;
 pub mod control;
