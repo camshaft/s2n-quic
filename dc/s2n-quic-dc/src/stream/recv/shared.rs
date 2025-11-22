d2248b7143285df023d601da2bad036d1ca9bb0a
Cameron Bytheway <bytheway.cameron@gmail.com>
2025-11-21 17:19:26 -0700

    fix warnings

--- dc/s2n-quic-dc/src/stream/recv/shared.rs
+++ dc/s2n-quic-dc/src/stream/recv/shared.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    clock,
     either::Either,
     event,
     packet::{stream, Packet},
--- dc/s2n-quic-dc/src/stream/recv/state.rs
+++ dc/s2n-quic-dc/src/stream/recv/state.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    clock,
     credentials::Credentials,
     crypto::{self, UninitSlice},
     event,
